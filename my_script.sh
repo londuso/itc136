bc1285253cc0888e18c59c145482c77da78d0f84
andy787@gmail.com <andy787@gmail.com>
2018-10-29 20:47:10 -0700

    I added some bash commands

--- my_script.sh
+++ my_script.sh
@@ -3,3 +3,12 @@
 echo "hello ITC136"
 
 echo "Edited in Github"
+
+echo "This is your current working directory:"
+pwd
+
+echo "This is the version of Linux you're running"
+uname -a
+
+echo "These are your logged in users:"
+who
