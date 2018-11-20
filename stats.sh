046a9bf52f73ebba50e07749736c557b6f6f6082
jetsetrez <38418756+jetsetrez@users.noreply.github.com>
2018-11-19 20:46:57 -0800

    Added colors

--- stats.sh
+++ stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-diskused=$( df -h | grep “/dev/sda1” | awk ‘{print $5}’ )
+diskused=$( df -h | grep "/dev/sda1" | awk '{print $5}' )
 
 memfree=$( free -mh | grep Mem: | awk '{print $7}' )
 
@@ -8,9 +8,9 @@ connections=$( netstat | grep tcp )
 
 users=$( who )
 
-echo "This is a snapshot of your current system:
+echo -e "This is a snapshot of your current system:
 
-      Disk Used: $diskused
-      Free Memory: $freemem
-      Open Internet Connections: $connections
-      Logged-in Users: $users"
+      \e[36m      Disk Used: $diskused
+      \e[35m      Free Memory: $memfree
+      \e[33m      Open Internet Connections: $connections
+      \e[32m      Logged-in Users: $users"
